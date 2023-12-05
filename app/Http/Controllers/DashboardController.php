bf5e3301a03fc943123ba2a4b99b2696e7a05b60
madep <mahfudade@gmail.com>
2023-12-05 22:59:23 +0700

    Add Dashboard view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
+use App\Models\Employees;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $companies = Company::all()->count();
+        $employees = Employees::all()->count();
+
         return view('dashboard', [
-            'title' => 'Dashboard'
+            'title' => 'Dashboard',
+            'companies' => $companies,
+            'employees' => $employees
         ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,7 +18,38 @@
                         <div class="card rounded-3 border-0 shadow-none">
                             <div class="card-body py-5">
                                 <div class="row justify-content-center">
-                                    @yield('content')
+                                    <div class="col-12 col-md-10">
+                                        <div class="row justify-content-center">
+                                            <div class="col-12 col-md-5">
+                                                <div class="small-box bg-primary">
+                                                    <div class="inner">
+                                                        <h3>{{ $companies }}</h3>
+                                                        <h5>Company</h5>
+                                                    </div>
+                                                    <div class="icon">
+                                                        <i class="ri-building-fill"></i>
+                                                    </div>
+                                                    <a href="/company" class="small-box-footer">
+                                                        Detail <i class="ri-arrow-right-s-line"></i>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-md-5">
+                                                <div class="small-box bg-success">
+                                                    <div class="inner">
+                                                        <h3>{{ $employees }}</h3>
+                                                        <h5>Employee</h5>
+                                                    </div>
+                                                    <div class="icon">
+                                                        <i class="ri-user-3-fill"></i>
+                                                    </div>
+                                                    <a href="/employees" class="small-box-footer">
+                                                        Detail <i class="ri-arrow-right-s-line"></i>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
